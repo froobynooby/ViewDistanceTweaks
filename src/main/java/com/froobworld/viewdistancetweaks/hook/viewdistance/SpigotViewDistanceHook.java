ffc93bf30729390c67b562d58f9cfcd5a2da6087
Dwayne Selsig <dselsig@gmail.com>
2023-12-08 16:02:29 +0100

    Update SpigotViewDistanceHook.java

--- src/main/java/com/froobworld/viewdistancetweaks/hook/viewdistance/SpigotViewDistanceHook.java
+++ src/main/java/com/froobworld/viewdistancetweaks/hook/viewdistance/SpigotViewDistanceHook.java
@@ -51,7 +51,7 @@ public class SpigotViewDistanceHook implements ViewDistanceHook {
         return NmsUtils.getMajorVersion() == 1
                 && NmsUtils.getMinorVersion() >= 18 // at least 1.18
                 && NmsUtils.getMinorVersion() <= 20 // no more than 1.20
-                && (NmsUtils.getMinorVersion() < 20 || NmsUtils.getRevisionNumber() <= 2); // no more than 1.20 R2
+                && (NmsUtils.getMinorVersion() < 20 || NmsUtils.getRevisionNumber() <= 3); // no more than 1.20 R3
     }
 
 }
